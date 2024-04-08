5fe47ac9651f3043025fe6768cdb2e620092ce6c
Giovanna-Cynthia <154370247+Giovanna-Cynthia@users.noreply.github.com>
2024-04-08 16:12:27 -0300

    08/04

--- API-Fetch/script.js
+++ API-Fetch/script.js
@@ -1,21 +1,47 @@
-//fetch ->
+//fetch => 
+
 //getPets()
 /*
 async function getPets() {
-//comando fetch usando async/await
-const reponse = await fetch("https://rickandmortyapi.com/api/character/?page=1")
-const pets = await reponse.json();
-console.log(pets.results)
+    //Comando fetch usando async/await
+    const response = await fetch("https://rickandmortyapi.com/api/character/?page=1")
+    const pets = await response.json()
+    console.log(pets.results)
 }
 */
 
-fetch("https://rickandmortyapi.com/api/character/?page=1")
-//then => então
-.then(rest => rest.json())
-.then(data => console.log(data))
+const urlRick = "https://rickandmortyapi.com/api/character/?page=1";
+const main = document.getElementById("container");
+
+fetch(urlRick, )
+    .then(rest => rest.json() )
+    .then(data => cards(data))
 
 function cards(data) {
 
-    console.log(data)
+    const dataArray = data.results;
+    //Criar os cards de forma automática
+
+    dataArray.forEach(element => {
+        const card = document.createElement("div");
+        card.className = "card"
+        card.innerHTML = `
+        <img src=${element.image} alt=${element.name}>
+        <div class="names">
+            <h3>Nome:</h3>
+            <h3>${element.name}</h3>
+        </div>
+        <div class="specie">
+            <h3>Espécie:</h3>
+            <h3>${element.species}</h3>
+        </div>
+        <div class="status">
+            <h3>Status:</h3>
+            <h3>${element.status}</h3>
+        </div>
+        `
+        
+        main.appendChild(card)
+    });
 
 }
